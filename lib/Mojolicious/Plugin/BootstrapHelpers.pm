c05f84cfc847587f883c1100ede645d655ac63dc
Erik Carlsson <info@code301.com>
2014-08-28 21:39:49 +0200

    init

---
+++ lib/Mojolicious/Plugin/BootstrapHelpers.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::BootstrapHelpers;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::BootstrapHelpers - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Mojolicious::Plugin::BootstrapHelpers;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::BootstrapHelpers is
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+use Mojolicious::Plugin::BootstrapHelpers;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
