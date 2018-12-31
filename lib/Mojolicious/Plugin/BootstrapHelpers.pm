9ff657a71aa0500713910c8b7d81d474c100b9c3
Erik Carlsson <info@code301.com>
2018-12-31 19:03:18 +0000

    Bump version after 0.0205

--- lib/Mojolicious/Plugin/BootstrapHelpers.pm
+++ lib/Mojolicious/Plugin/BootstrapHelpers.pm
@@ -5,7 +5,7 @@ package Mojolicious::Plugin::BootstrapHelpers;
 
 # ABSTRACT: Type less bootstrap
 # AUTHORITY
-our $VERSION = '0.0205';
+our $VERSION = '0.0206';
 
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojolicious::Plugin::BootstrapHelpers::Helpers;
